8f0e04d05cede5f5df1823348fe9c9523b6d88b7
MatanWaisman <matan.waisman@gmail.com>
2020-03-05 22:47:01 +0200

    Updates

---
+++ precache-manifest.330ca3b8d866b36a0c09c2e1c76b7874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d20183784cbedfb727119e5ed6f979",
+    "url": "/SmartHouse/index.html"
+  },
+  {
+    "revision": "f32ac51899300a3b6858",
+    "url": "/SmartHouse/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5c9bef08c720b9698a88",
+    "url": "/SmartHouse/static/js/2.072949e4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/SmartHouse/static/js/2.072949e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f32ac51899300a3b6858",
+    "url": "/SmartHouse/static/js/main.5a138810.chunk.js"
+  },
+  {
+    "revision": "6320abf15ad308cd70a1",
+    "url": "/SmartHouse/static/js/runtime-main.a4cfff39.js"
+  }
+]);
